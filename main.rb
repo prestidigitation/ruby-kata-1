dc715c6a968718513d51b8a7d120221265bf0d41
Alexander Rowland <rowlanda@protonmail.com>
2022-11-10 15:42:35 -0800

    Implement find_book_by_isbn, find_magazine_by_isbn

--- main.rb
+++ main.rb
@@ -36,3 +36,28 @@ magazines.each_with_index do |magazine, i|
   puts "Publication Date: #{magazine[:publishedat]}"
   puts "=" * 100 unless magazines[i + 1].nil?
 end
+
+def find_book_by_isbn(isbn, books)
+  found = nil
+  books.each do |book|
+    if book[:isbn] == isbn
+      found = book
+      break
+    end
+  end
+  found
+end
+
+def find_magazine_by_isbn(isbn, magazines)
+  found = nil
+  magazines.each do |magazine|
+    if magazine[:isbn] == isbn
+      found = magazine
+      break
+    end
+  end
+  found
+end
+
+p find_book_by_isbn("2145-8548-3325", books)
+p find_magazine_by_isbn("2365-8745-7854", magazines)
