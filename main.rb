94ca70f7d19e451fabf2c3cd2e63b38394721c90
Alexander Rowland <rowlanda@protonmail.com>
2022-11-10 16:05:47 -0800

    Put all logic into one big Parser god class

--- main.rb
+++ main.rb
@@ -1,49 +1,64 @@
 require "csv"
 
-authors = CSV.read(
-  "data/authors.csv",
-  headers: true,
-  header_converters: :symbol,
-  col_sep: ";"
-).map(&:to_h)
+class Parser
+  def initialize(authors_path:, books_path:, magazines_path:)
+    @authors = CSV.read(
+      authors_path,
+      headers: true,
+      header_converters: :symbol,
+      col_sep: ";"
+    ).map(&:to_h)
+    
+    @books = CSV.read(
+      books_path,
+      headers: true,
+      header_converters: :symbol,
+      col_sep: ";"
+    ).map(&:to_h)
 
-books = CSV.read(
-  "data/books.csv",
-  headers: true,
-  header_converters: :symbol,
-  col_sep: ";"
-).map(&:to_h)
+    @magazines = CSV.read(
+      magazines_path,
+      headers: true,
+      header_converters: :symbol,
+      col_sep: ";"
+    ).map(&:to_h)
+  end
 
-magazines = CSV.read(
-  "data/magazines.csv",
-  headers: true,
-  header_converters: :symbol,
-  col_sep: ";"
-).map(&:to_h)
+  def find_book_by_isbn(isbn)
+    @books.find { |book| book[:isbn] == isbn}
+  end
+  
+  def find_magazine_by_isbn(isbn)
+    @magazines.find { |magazine| magazine[:isbn] == isbn }
+  end
 
-books.each_with_index do |book, i|
-  puts "Title: #{book[:title]}"
-  puts "ISBN: #{book[:isbn]}"
-  puts "Authors: #{book[:authors]}"
-  puts "Description: #{book[:description]}"
-  puts "=" * 100 unless books[i + 1].nil?
+  def print_all_books
+    @books.each_with_index do |book, i|
+      puts "Title: #{book[:title]}"
+      puts "ISBN: #{book[:isbn]}"
+      puts "Authors: #{book[:authors]}"
+      puts "Description: #{book[:description]}"
+      puts "=" * 100 unless @books[i + 1].nil?
+    end
+  end
+
+  def print_all_magazines
+    @magazines.each_with_index do |magazine, i|
+      puts "Title: #{magazine[:title]}"
+      puts "ISBN: #{magazine[:isbn]}"
+      puts "Authors: #{magazine[:authors]}"
+      puts "Publication Date: #{magazine[:publishedat]}"
+      puts "=" * 100 unless @magazines[i + 1].nil?
+    end
+  end
 end
 
-magazines.each_with_index do |magazine, i|
-  puts "Title: #{magazine[:title]}"
-  puts "ISBN: #{magazine[:isbn]}"
-  puts "Authors: #{magazine[:authors]}"
-  puts "Publication Date: #{magazine[:publishedat]}"
-  puts "=" * 100 unless magazines[i + 1].nil?
-end
+parser = Parser.new(
+  authors_path: "data/authors.csv",
+  books_path: "data/books.csv",
+  magazines_path: "data/magazines.csv"
+)
 
-def find_book_by_isbn(isbn, books)
-  books.find { |book| book[:isbn] == isbn}
-end
-
-def find_magazine_by_isbn(isbn, magazines)
-  magazines.find { |magazine| magazine[:isbn] == isbn }
-end
-
-p find_book_by_isbn("2145-8548-3325", books)
-p find_magazine_by_isbn("2365-8745-7854", magazines)
+# p parser.find_book_by_isbn("2145-8548-3325")
+# p parser.find_magazine_by_isbn("2365-8745-7854")
+parser.print_all_books
